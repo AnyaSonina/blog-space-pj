7dc7bf927cf3e4364757d41ec00d361e56a61dbe
AnyaSonina <89742951+AnyaSonina@users.noreply.github.com>
2023-04-11 15:06:38 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+let postsArray = []
+const form = document.getElementById("form")
+const titleInput = document.getElementById("post__title")
+const bodyInput = document.getElementById("post__body")
+let posts = JSON.parse(localStorage.getItem("posts"))
+
+document.addEventListener("click", function(e) {
+  if(e.target.dataset.id) {
+    handleClick(e.target.dataset.id)
+  }
+})
+
+function renderPost() {
+  let html = ""
+  
+  if(posts.length >= 1) {
+    postsArray = posts
+  }
+
+  for (let post of postsArray) {
+    html += `<div class="post"><h1>${post.title}</h1>
+    <p>${post.body}</p>
+    <button data-id="${post.id}" class="post_btn" >Delete</button></div>` 
+  }
+  document.getElementById("posts__list").innerHTML = html
+}
+
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+.then(res => res.json())
+.then(data => {
+  postsArray = data.slice(0,5)
+  renderPost()
+})
+
+
+form.addEventListener("submit", function(event){
+event.preventDefault()
+
+const postTitle = titleInput.value
+const postBody = bodyInput.value
+const dataObject = {
+  title: postTitle,
+  body: postBody 
+}
+
+const options = {
+  method: "POST",
+  body: JSON.stringify(dataObject),
+  headers: {
+    "Content-type":"application/json"
+  }
+}
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+.then(res => res.json())
+.then(post => {
+  if(post.title && post.body)
+  postsArray.unshift(post)
+  localStorage.setItem("posts", JSON.stringify(postsArray))
+  renderPost()
+  form.reset()
+})
+})
+
+function handleClick(id) {
+  let filteredArr = postsArray.filter(post => {
+    return post.id == id
+  })[0]
+  let index = postsArray.indexOf(filteredArr)
+  postsArray.splice(index, 1)
+  localStorage.setItem("posts", JSON.stringify(postsArray))
+  renderPost()
+}
